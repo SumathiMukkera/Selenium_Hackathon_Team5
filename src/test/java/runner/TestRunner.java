b1359e262381ddb9a238b7be50884e3d428fc45e
SumathiMukkera <sumathi.barupati9@gmail.com>
2025-02-25 11:20:13 -0600

    new changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,14 +9,15 @@ import io.cucumber.testng.CucumberOptions;
 import utilitities.ConfigReader;
 
 
-@CucumberOptions(features = "src/test/resources/featureFiles", glue = { "stepDefinitions",
+@CucumberOptions(features = "src/test/resources/featureFiles/ProgramPagination.feature", glue = { "stepDefinitions",
 		"hooks" }, plugin = { "pretty", "html:src/test/resources/cucumber-reports.html",
 				"json:src/test/resources/cucumber-reports.json", 
 				"com.aventstack.chaintest.plugins.ChainTestCucumberListener:",
 			"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
 //				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" 
+				 tags = "@test2",
 		monochrome = true)
-		 //tags = "@logout")
+		
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 //	@BeforeTest
